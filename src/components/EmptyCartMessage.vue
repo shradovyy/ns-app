6f848212ba84eae767e28ae2752dede144a23d7d
shradovyy <shradovyy@gmail.com>
2021-03-18 16:00:10 +0200

    Cart message component

---
+++ src/components/EmptyCartMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <FlexboxLayout class="message">
+        <Image src="~/images/shopping-bag.png" class="icon"/>
+        <Label text="Your cart is empty" class="text"/>
+    </FlexboxLayout>
+</template>
+
+<script>
+export default {
+    name: 'EmptyCartMessage'
+}
+</script>
+
+<style scoped>
+    .message {
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        height: 500;
+    }
+
+    .icon {
+        width: 120;
+        margin-bottom: 30;
+        opacity: 0.4;
+    }
+
+</style>
